f8f36b1a4c8b2fefa926b978cf0dcf8cbc3a40cf
Prosanto7 <prosantodeb7@gmail.com>
2024-04-11 22:51:20 +0600

    Prediction part completed

--- GUI Part/HomePage.py
+++ GUI Part/HomePage.py
@@ -1,6 +1,7 @@
 from customtkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+from CTkMessagebox import CTkMessagebox
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,6 +27,7 @@ class HomePageApp(CTk):
         self.graph_image = CTkImage(Image.open(current_path + "\\assets\\images\\Graph.png"), size = (30, 30))
         self.logout_image = CTkImage(Image.open(current_path + "\\assets\\images\\Logout.png"), size = (30, 35))
         self.dataset_file_path = "assets\\dataset\\sales_data.csv"
+        self.model_file_path = "assets\\model\\model.pickle"
        
 
     def setCenterWindow(self):
@@ -76,22 +78,42 @@ class HomePageApp(CTk):
 
         self.load_file_frame = CTkFrame(master = self, corner_radius = 0, fg_color = "transparent")
         self.load_file_button = CTkButton(master = self.load_file_frame, text = "Load File", command = self.load_file_button_event)
-        self.load_file_button.pack(pady = 10)
+        self.load_file_button.pack(pady = (20, 0))
         self.file_description_label = CTkLabel(master = self.load_file_frame)
-        self.file_description_label.pack(pady = 10)
+        self.file_description_label.pack(pady = 20)
         self.fig, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(figure = self.fig, master = self.load_file_frame)
-        self.toolbar = NavigationToolbar2Tk(canvas = self.canvas, window = self.load_file_frame, pack_toolbar = FALSE)
-        self.toolbar.pack(pady = 10, anchor = "w", fill = "x")
-
         self.canvas.get_tk_widget().pack()
+        self.toolbar = NavigationToolbar2Tk(canvas = self.canvas, window = self.load_file_frame, pack_toolbar = FALSE)
+        self.toolbar.pack(pady = 20, anchor = "w", fill = "x")
         plt.close(self.fig)
 
         self.generate_model_frame = CTkFrame(master = self, corner_radius = 0, fg_color = "transparent")
-        CTkButton(master = self.generate_model_frame, text = "Generate Model").pack(pady = 10)
+        CTkButton(master = self.generate_model_frame, text = "Generate Model", command = self.generate_model_button_event).pack(pady = (20, 0))
+        self.model_description_label = CTkLabel(master = self.generate_model_frame)
+        self.model_description_label.pack(pady = 20)
 
-        self.predict_sales_frame = CTkFrame(master = self, corner_radius = 0, fg_color = "transparent")
-        CTkButton(master = self.predict_sales_frame, text = "Predict Sales").pack(pady = 10)
+
+        self.predict_sales_frame = CTkScrollableFrame(master = self, corner_radius = 0, fg_color = "transparent")
+        self.prediction_description_label = CTkLabel(master = self.predict_sales_frame)
+        self.prediction_description_label.pack(pady = (20, 0))
+
+        selection_frame = CTkFrame(master = self.predict_sales_frame, fg_color="transparent")
+        selection_frame.pack(pady = 20)
+
+        self.unit_menu = CTkOptionMenu(master = selection_frame, command=self.predict_sales_event)
+        self.unit_menu.set("Select A Unit to Predict    ")
+        self.unit_menu.pack(side = "left", padx = 20)
+
+        self.predicted_sales_label = CTkLabel(master = selection_frame, text = "Predicted sales amount : ")
+        self.predicted_sales_label.pack(side = "right")
+        
+        self.predict_fig, self.predict_ax = plt.subplots()
+        self.predict_canvas = FigureCanvasTkAgg(figure = self.predict_fig, master = self.predict_sales_frame)
+        self.predict_canvas.get_tk_widget().pack()
+        self.predict_toolbar = NavigationToolbar2Tk(canvas = self.predict_canvas, window = self.predict_sales_frame, pack_toolbar = FALSE)
+        self.predict_toolbar.pack(pady = 20, anchor = "w", fill = "x")
+        plt.close(self.predict_fig)
 
         # select default frame
         self.select_frame_by_name("load_file")
@@ -106,7 +128,11 @@ class HomePageApp(CTk):
             if os.path.exists(self.dataset_file_path):
                 self.file_description_label.configure(text = "Dataset already exists!")
                 df = pd.read_csv(self.dataset_file_path)
-                self.ax.plot(df["Month"], df["Sales"])
+                df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
+                self.ax.plot(df[df.columns[0]], df[df.columns[1]])
+                self.ax.set_xlabel(df.columns[0])
+                self.ax.set_ylabel(df.columns[1])
+                self.ax.set_title('Data Plot')
                 self.canvas.draw()
                 self.toolbar.update()
 
@@ -118,11 +144,42 @@ class HomePageApp(CTk):
 
         if name == "generate_model":
             self.generate_model_frame.grid(row = 0, column = 1, sticky = "nsew")
+            if os.path.exists(self.model_file_path):
+                self.model_description_label.configure(text = "Generated model already exists in your system!!!")
+            else:
+                self.model_description_label.configure(text = "Please generate a model for prediction.")
         else:
             self.generate_model_frame.grid_forget()
 
         if name == "predict_sales":
             self.predict_sales_frame.grid(row = 0, column = 1, sticky = "nsew")  
+            if os.path.exists(self.model_file_path):
+                self.prediction_description_label.configure(text = "Generated model already exists in your system!!!")
+
+                import pickle
+                with open(self.model_file_path, 'rb') as f:
+                    loaded_model = pickle.load(f)
+
+                from pandas.tseries.offsets import DateOffset
+                df = pd.read_csv(self.dataset_file_path)
+                df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
+                df.set_index(df.columns[0], inplace = True)
+                future_dates=[df.index[-1]+ DateOffset(months=x)for x in range(0,24)]
+                future_datest_df=pd.DataFrame(index=future_dates[1:],columns=df.columns)
+                self.future_df=pd.concat([df,future_datest_df])
+                self.future_df['forecast'] = loaded_model.predict(start = df.size - 3, end = df.size + 24, dynamic= True)  
+                self.predict_ax.plot(self.future_df['Sales'])
+                self.predict_ax.plot(self.future_df['forecast'])
+                self.predict_ax.set_xlabel("Date")
+                self.predict_ax.set_ylabel("Sales")
+                self.predict_ax.set_title('Prediction Plot')
+                self.predict_canvas.draw()
+                self.predict_toolbar.update()
+
+                future_dates_str = [date.strftime('%d / %m / %Y') for date in future_dates]
+                self.unit_menu.configure(values = future_dates_str)
+            else:
+                self.prediction_description_label.configure(text = "Please generate a model for prediction.")
         else:
             self.predict_sales_frame.grid_forget()     
 
@@ -141,8 +198,37 @@ class HomePageApp(CTk):
     def generate_model_nav_button_event(self):
         self.select_frame_by_name("generate_model")
 
+    def generate_model_button_event(self):
+        if os.path.exists(self.dataset_file_path):
+            import statsmodels.api as sm
+            import pickle
+
+            df = pd.read_csv(self.dataset_file_path)
+            df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
+            df.set_index(df.columns[0], inplace = True)
+            sarima = sm.tsa.statespace.SARIMAX(df['Sales'],order = (1, 1, 1),seasonal_order = (1,1,1,12))
+            model = sarima.fit()
+            with open(self.model_file_path, 'wb') as f:
+                pickle.dump(model, f)
+
+            self.model_description_label.configure(text = "Model generated successfully.")
+            CTkMessagebox(title="Success", message = "Model generated successfully!!!", icon = "check") 
+        else:
+            CTkMessagebox(title = "Warning", message = "Dataset not found!!!\nPlease load your dataset in Sales Predictor.", icon = "cancel") 
+   
+
     def predict_sales_nav_button_event(self):
-        self.select_frame_by_name("predict_sales")         
+        self.select_frame_by_name("predict_sales")      
+
+    def predict_sales_event(self, choice):
+        from pandas.tseries.offsets import DateOffset
+        df = pd.read_csv(self.dataset_file_path)
+        df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
+        df.set_index(df.columns[0], inplace = True)
+        future_dates=[df.index[-1]+ DateOffset(months=x)for x in range(0,24)]
+        future_dates_str = [date.strftime('%d / %m / %Y') for date in future_dates]
+        date_index = future_dates_str.index(choice)
+        self.predicted_sales_label.configure(text = "Predicted sales amount : " + str(round(self.future_df.iloc[df.size + date_index - 1].forecast, 2)))
 
     def logout_button_event(self):
         self.destroy()
@@ -155,7 +241,7 @@ class HomePageApp(CTk):
         loginPageApp.mainloop()
 
 
-homePageApp = HomePageApp(width = 800, height = 600, title = "Sales Predictor")
-homePageApp.setCenterWindow()
-homePageApp.setComponents()
-homePageApp.mainloop()
+# homePageApp = HomePageApp(width = 800, height = 600, title = "Sales Predictor")
+# homePageApp.setCenterWindow()
+# homePageApp.setComponents()
+# homePageApp.mainloop()
